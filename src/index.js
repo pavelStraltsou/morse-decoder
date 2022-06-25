61a6e47ef0fca9d18dcdea0ab2e4d5d82e495af8
PaulSensor <sensor445@gmail.com>
2022-06-25 22:43:57 +0300

    feat: add solution task Morse code decoder in index.js

--- src/index.js
+++ src/index.js
@@ -38,7 +38,33 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arrExpr = []
+    let finalStr = ''
+
+    for (let i = 0; i < expr.length; i++) {
+        if (i % 10 === 0) {
+            arrExpr.push(expr.slice(i, i + 10))
+        }
+    }
+
+    let arrMorse = arrExpr.map(i => {
+        for (let u = 0; u < i.length; u++) {
+            if (u % 2 === 0 && i[u] + i[u + 1] === '00') {
+                i += ''
+            } else if (u % 2 === 0 && i[u] + i[u + 1] === '10') {
+                i += '.'
+            } else if (u % 2 === 0 && i[u] + i[u + 1] === '11') {
+                i += '-'
+            }
+        }
+        return MORSE_TABLE[`${i.slice(10)}`] 
+    })
+
+    for (let j = 0; j < arrMorse.length; j++) {
+        arrMorse[j] ? finalStr += arrMorse[j] : finalStr += ' '
+    }
+    
+    return finalStr
 }
 
 module.exports = {
